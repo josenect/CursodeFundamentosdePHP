e9008780bc1292626ceedfcf5588670b0c06e24e
Jose Rodriguez <josenect@hotmail.es>
2020-12-03 11:23:32 -0500

    bloques de Codigo

---
+++ bloques.php
@@ -0,0 +1,51 @@
+<?php
+
+
+if (condition) {
+    # code...
+} elseif (condition) {
+    # code...
+
+}else {
+    # code...
+}
+
+
+switch ($variable) {
+    case 'value':
+        # code...
+        break;
+    
+    case 'value':
+         # code...
+         break;
+
+      case 'value':
+         # code...
+         break;     
+
+    default:
+        # code...
+        break;
+}
+
+
+foreach ($datos as $datos.id => $datos.name) {
+    # code...
+}
+
+ $a =1 ;
+while ($a <= 10) {
+    # code...
+    $a++;
+}
+
+do {
+    # code...
+} while ($a <= 10);
+
+for ($i=0; $i < 10; $i++) { 
+    # code...
+
+    print $i ;
+}
