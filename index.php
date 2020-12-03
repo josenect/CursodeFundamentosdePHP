55d9f86cf4331cc7220bf2da586b482c806eff90
Jose Rodriguez <josenect@hotmail.es>
2020-12-03 10:31:54 -0500

    Introduccion a PHP Sintaxis de comentario

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+//sintaxis de php
+#sintaxis de php
+
+/*Bloque de 
+comentario 
+*/
+
+class Comun
+{
+    /**
+     * Comentario para variables
+     * lineas para 
+     * palabras 
+     */
+    protected $Clients=[];
+    protected $Persona=[];
+
+    
+}
+
+
+
+
+
+
+
+
+
+
