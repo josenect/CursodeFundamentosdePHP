c27307ac6594f5b326f084352b3c38259ffa04fa
Jose Rodriguez <josenect@hotmail.es>
2020-12-03 10:52:26 -0500

    Sintaxis Basica Asignacion y Aritmetica

---
+++ SintaxisBasica.php
@@ -0,0 +1,21 @@
+<?php
+
+
+// Asignación
+
+$num =9 ;
+$lang =[
+     'es' => 'español',
+     'en' => 'ingles'
+];
+
+
+//Aritmética
+
+echo "suma 2 +2 = " .((int)2 + (int)2) ." <br>";
+echo "Resta 2 - 2 = " .((int)2 - (int)2)." <br>";
+echo "Multiplicacion 2 * 2 = " . 2 * 2 ." <br>";
+echo "Divide 2 / 2 = " . 2 / 2 ." <br>";
+echo "modulo 2 % 2 = " . 2 % 2 ." <br>";
+echo "Divide 2 ** 2 = " . 2 ** 2 ." <br>";
+
