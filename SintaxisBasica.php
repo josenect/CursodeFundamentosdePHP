0f6173f5e3b59d8906b540227974a35a1b429bcd
Jose Rodriguez <josenect@hotmail.es>
2020-12-03 11:12:47 -0500

    Sintaxis Basica comparacion y variables variables

--- SintaxisBasica.php
+++ SintaxisBasica.php
@@ -12,10 +12,26 @@ $lang =[
 
 //Aritmética
 
-echo "suma 2 +2 = " .((int)2 + (int)2) ." <br>";
+echo "suma 2 +2 = " . (2 + 2)  ." <br>";
 echo "Resta 2 - 2 = " .((int)2 - (int)2)." <br>";
 echo "Multiplicacion 2 * 2 = " . 2 * 2 ." <br>";
 echo "Divide 2 / 2 = " . 2 / 2 ." <br>";
 echo "modulo 2 % 2 = " . 2 % 2 ." <br>";
 echo "Divide 2 ** 2 = " . 2 ** 2 ." <br>";
+
+
+//comparacion 
+/**
+ * igual == ,valor '9' == 9
+ * igual === ,valor - tipo 9 === 9
+ * Diferencias !=, valor 
+ * Diferencias !==, valor - tipo 
+ *  < ,> <= >= 
+ */
+//variables
+$app = 'name';
+$name = 'platzi' ;
+
+echo $app .'<br>' ;
+echo $$app;    
 
